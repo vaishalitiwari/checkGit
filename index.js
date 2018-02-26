884e7dae3e2a41902b3b70a386a64bfe20ae2c21
Vaishali Tiwari <vaishali.tiwari@bnsf.com>
2018-02-25 22:29:54 -0600

    changes to deploy

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port =  process.env.PORT || 1234;
 
 app.get('/',(req,res)=>{
   res.send('Hey There !!');
 });
 
 app.listen(1234,()=>{
-  console.log('server is up at port 1234');
+  console.log(`server is up at port ${port}`);
 })
